2b799755825fa7cd86a4fe1548fda6e42e1e3614
anish <anees.manandhar@gmail.com>
2015-12-31 16:22:42 +0545

    sample test

---
+++ src/main/java/org/akm/integration/report/App.java
@@ -0,0 +1,42 @@
+package org.akm.integration.report;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	
+	public static void main(String[] args) {
+		run();
+	}
+	
+	private static void run() {
+
+		String[] springConfig = { "batch/archive-job.xml" };
+
+		ApplicationContext context = new ClassPathXmlApplicationContext(springConfig);
+
+		JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
+		Job job = (Job) context.getBean("archiveJob");
+
+		try {
+
+			JobParameters param = new JobParametersBuilder().addString("param", "param").toJobParameters();
+			
+			JobExecution execution = jobLauncher.run(job, param);
+			System.out.println("Exit Status : " + execution.getStatus());
+			System.out.println("Exit Status : " + execution.getAllFailureExceptions());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+		System.out.println(context.getApplicationName());
+		System.out.println("Done");
+
+	}
+}
