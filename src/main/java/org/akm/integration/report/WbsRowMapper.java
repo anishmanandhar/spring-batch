b7c51663671945b6e1aa07066e20812d5290190f
anish <anees.manandhar@gmail.com>
2016-01-01 14:48:13 +0545

    Added transaction manager in mysql batch processing

--- src/main/java/org/akm/integration/report/WbsRowMapper.java
+++ src/main/java/org/akm/integration/report/WbsRowMapper.java
@@ -3,6 +3,8 @@ package org.akm.integration.report;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.RowMapper;
 
 /**
@@ -12,19 +14,16 @@ import org.springframework.jdbc.core.RowMapper;
  */
 public class WbsRowMapper implements RowMapper<WbsElementDto>{
 
-	private static int count = 0;
+	private static final Logger LOG = LoggerFactory.getLogger(WbsRowMapper.class);
 	
 	public WbsElementDto mapRow(ResultSet arg0, int arg1) throws SQLException {
-		System.out.println("Mapping wbs elements");
 		WbsElementDto wbsElementDto = new WbsElementDto();
 		wbsElementDto.setClientId(arg0.getString(1));
 		wbsElementDto.setCode(arg0.getString(2));
 		wbsElementDto.setDescription(arg0.getString(3));
 		wbsElementDto.setCompanyCode(arg0.getString(4));
 		
-		count++;
-		System.out.println(count);
-		System.out.println(wbsElementDto);
+		LOG.info("{}",wbsElementDto);
 		return wbsElementDto;
 	}
 
