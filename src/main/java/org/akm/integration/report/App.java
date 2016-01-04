3baa5319007f83496fe19d9f5b57e80541424a3a
anish <anees.manandhar@gmail.com>
2016-01-04 11:08:26 +0545

    added delete job after successfull insertion for archiving

--- src/main/java/org/akm/integration/report/App.java
+++ src/main/java/org/akm/integration/report/App.java
@@ -1,14 +1,19 @@
 package org.akm.integration.report;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(App.class);
 	
 	public static void main(String[] args) {
 		run();
@@ -26,17 +31,15 @@ public class App {
 		try {
 
 			JobParameters param = new JobParametersBuilder().addString("param", "param").toJobParameters();
-			
 			JobExecution execution = jobLauncher.run(job, param);
-			System.out.println("Exit Status : " + execution.getStatus());
-			System.out.println("Exit Status : " + execution.getAllFailureExceptions());
-
+			LOG.info("Exit Status : " + execution.getStatus());
+			LOG.info("Exit Status : " + execution.getAllFailureExceptions());
 		} catch (Exception e) {
 			e.printStackTrace();
 
 		}
-		System.out.println(context.getApplicationName());
-		System.out.println("Done");
+		((ConfigurableApplicationContext)context).close();
+		LOG.info("Done");
 
 	}
 }
--- src/main/java/org/akm/integration/report/WbsRowMapper.java
+++ src/main/java/org/akm/integration/report/WbsRowMapper.java
@@ -12,17 +12,18 @@ import org.springframework.jdbc.core.RowMapper;
  * @author anish
  *
  */
-public class WbsRowMapper implements RowMapper<WbsElementDto>{
+public class WbsRowMapper extends Thread implements RowMapper<WbsElementDto>{
 
 	private static final Logger LOG = LoggerFactory.getLogger(WbsRowMapper.class);
 	
 	public WbsElementDto mapRow(ResultSet arg0, int arg1) throws SQLException {
+		LOG.info("#####################################################################");
 		WbsElementDto wbsElementDto = new WbsElementDto();
 		wbsElementDto.setClientId(arg0.getString(1));
 		wbsElementDto.setCode(arg0.getString(2));
 		wbsElementDto.setDescription(arg0.getString(3));
 		wbsElementDto.setCompanyCode(arg0.getString(4));
-		
+		LOG.info(String.format("execute thread: %s %s",Thread.currentThread().getName(), this.getName()));
 		LOG.info("{}",wbsElementDto);
 		return wbsElementDto;
 	}
